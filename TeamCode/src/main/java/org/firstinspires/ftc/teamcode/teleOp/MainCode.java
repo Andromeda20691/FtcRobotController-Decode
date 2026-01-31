c562fc7ba13ce9a2aeeb048604d891bf679e2728
OguzSalgur <andromeda@isb.ro>
2026-01-31 11:12:42 +0200

    fixup! MainUpload

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/MainCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/MainCode.java
@@ -23,13 +23,12 @@ public class MainCode extends OpMode
 
     private DcMotor outtakeTop;
     private DcMotor outtakeBottom;
-    private DcMotor intakeMotor1;
-    private DcMotor intakeMotor2;
+    private DcMotor intakeMotor;
 
     private Servo sorterServo;
     private Servo shooterServo;
 
-    // private NormalizedColorSensor colorSensor;
+    private NormalizedColorSensor colorSensor;
 
     private double sorterPos1 = 1.0;
     private double sorterPos2 = 0.5;
@@ -53,7 +52,9 @@ public class MainCode extends OpMode
     boolean outtakeOn = false;
     boolean lastx = false;
     boolean lastb = false;
-    boolean intakeActivated = false;
+
+
+
     private double denominator = 0;
     private double frontLeftPower =0, backLeftPower =0, frontRightPower =0, backRightPower =0;
     private double y =0, x =0, rx =0;
@@ -70,22 +71,22 @@ public class MainCode extends OpMode
 
         outtakeBottom = hardwareMap.dcMotor.get("outtakeBottom");
         outtakeTop = hardwareMap.dcMotor.get("outtakeTop");
-        intakeMotor1 = hardwareMap.dcMotor.get("intakeMotor1");
-        intakeMotor2 = hardwareMap.dcMotor.get("intakeMotor2");
+        intakeMotor = hardwareMap.dcMotor.get("intakeMotor");
 
         sorterServo = hardwareMap.servo.get("sorterServo");
-        shooterServo = hardwareMap.servo.get("shooterServo");
+        shooterServo =hardwareMap.servo.get("shooterServo");
 
 
-        // colorSensor = hardwareMap.get(NormalizedColorSensor.class, "colorSensor");
-        // colorSensor.setGain(40);
+        colorSensor = hardwareMap.get(NormalizedColorSensor.class, "colorSensor");
+        colorSensor.setGain(40);
 
         // TODO: 1/9/2026 here chage the ction of the outake motors they are prob wronge because i didnt have time to check them
-        outtakeTop.setDirection(DcMotorSimple.Direction.REVERSE);
-        outtakeBottom.setDirection(DcMotorSimple.Direction.FORWARD);
+        outtakeTop.setDirection(DcMotorSimple.Direction.FORWARD);
+        outtakeBottom.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        intakeMotor1.setDirection(DcMotorSimple.Direction.REVERSE);
-        intakeMotor2.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        intakeMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
 
         rightFront.setDirection(DcMotor.Direction.FORWARD);
         rightBack.setDirection(DcMotor.Direction.FORWARD);
@@ -97,16 +98,10 @@ public class MainCode extends OpMode
         leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        intakeMotor1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        intakeMotor2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
         rightFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         rightBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         leftFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         leftBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
-        intakeMotor1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        intakeMotor2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
@@ -115,29 +110,25 @@ public class MainCode extends OpMode
     @Override
     public void loop() {
 
-        if (gamepad1.right_bumper) {
+        if(gamepad1.right_bumper) {
             speedMultiplier = 0.25;
         } else {
             speedMultiplier = 1;
         }
-
         y = -gamepad1.left_stick_y;
-        x = gamepad1.left_stick_x;
+        x = -gamepad1.left_stick_x;
         rx = gamepad1.right_stick_x;
-
         denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-
         frontLeftPower = (y + x + rx) / denominator * speedMultiplier;
         backLeftPower = (y - x + rx) / denominator * speedMultiplier;
         frontRightPower = (y - x - rx) / denominator * speedMultiplier;
         backRightPower = (y + x - rx) / denominator * speedMultiplier;
-
         leftFront.setPower(frontLeftPower);
         leftBack.setPower(backLeftPower);
         rightFront.setPower(frontRightPower);
         rightBack.setPower(backRightPower);
 
-        // NormalizedRGBA colors = colorSensor.getNormalizedColors();
+        NormalizedRGBA colors = colorSensor.getNormalizedColors();
 
         sorterServoPos = sorterServo.getPosition();
 
@@ -148,16 +139,17 @@ public class MainCode extends OpMode
         }
         lastb = outtakeToggleButton;
 
-        if (outtakeOn) {
+        if (outtakeOn){
             outtakeTop.setPower(1);
             outtakeBottom.setPower(1);
-        } else {
+        }else {
             outtakeTop.setPower(0);
             outtakeBottom.setPower(0);
         }
+
+
         boolean yPressed = gamepad1.y;
         boolean aPressed = gamepad1.a;
-
 
         /* MOVE DOWN (Y) */
         if (yPressed && !lastY) {
@@ -181,9 +173,10 @@ public class MainCode extends OpMode
             shooterServo.setPosition(shooterCurrentPos);
         }
 
-        /*
         lastY = yPressed;
         lastA = aPressed;
+
+
 
 
         boolean intakeToggleButton = gamepad1.x;
@@ -209,124 +202,38 @@ public class MainCode extends OpMode
         }
 
 
-        intakeMotor1.setPower(finalIntakePower);*/
+        intakeMotor.setPower(finalIntakePower);
 
-        double stickIntakePower = -gamepad1.right_stick_y;
 
-        intakeMotor1.setPower(stickIntakePower);
-        intakeMotor2.setPower(stickIntakePower);
 
-        if (gamepad1.dpad_down && !intakeActivated) {
+        
 
-            intakeActivated = true;
-            displayTelemetry();
-
-            intakeMotor1.setPower(1);
-            intakeMotor2.setPower(1);
-
-            try {
-
-                Thread.sleep(250);
-
-            } catch (InterruptedException e) {
-
-                telemetry.addLine("An exception has occurred within the code :(");
-
-            }
-
-        } else if (gamepad1.dpad_down && intakeActivated) {
-
-            intakeActivated = false;
-            displayTelemetry();
-
-            intakeMotor1.setPower(0);
-            intakeMotor2.setPower(0);
-
-            try {
-
-                Thread.sleep(250);
-
-            } catch (InterruptedException e) {
-
-                telemetry.addLine("An exception has occurred within the code :(");
-
-            }
-
-        }
-
-        if (gamepad1.dpad_up && !intakeActivated) {
-
-            sorterServo.setPosition(sorterPos2);
-
-            intakeActivated = true;
-            displayTelemetry();
-
-            intakeMotor1.setPower(-0.5);
-            intakeMotor2.setPower(-0.5);
-
-            try {
-
-                Thread.sleep(250);
-
-            } catch (InterruptedException e) {
-
-                telemetry.addLine("An exception has occurred within the code :(");
-
-            }
-
-        } else if (gamepad1.dpad_up && intakeActivated) {
-
-            sorterServo.setPosition(sorterPos2);
-
-            intakeActivated = false;
-            displayTelemetry();
-
-            intakeMotor1.setPower(0);
-            intakeMotor2.setPower(0);
-
-            try {
-
-                Thread.sleep(250);
-
-            } catch (InterruptedException e) {
-
-                telemetry.addLine("An exception has occured within the code :(");
-
-            }
-
-        }
-
-        if (gamepad1.dpad_left) {
+        if (gamepad1.dpad_left){
             sorterServo.setPosition(sorterPos1);
         }
-        if (gamepad1.dpad_right) {
+        if (gamepad1.dpad_up){
+            sorterServo.setPosition(sorterPos2);
+        }
+        if (gamepad1.dpad_right){
             sorterServo.setPosition(sorterPos3);
         }
 
-        displayTelemetry();
 
-    }
 
-    public void displayTelemetry(){
-
-        telemetry.addData("leftFrontPower: ", leftFront.getPower());
-        telemetry.addData("leftBackPower: ", leftBack.getPower());
-        telemetry.addData("rightBackPower: ", rightBack.getPower());
-        telemetry.addData("rightFrontPower: ", rightFront.getPower());
+        telemetry.addData("leftFrontPower",leftFront.getPower());
+        telemetry.addData("leftBackPower",leftBack.getPower());
+        telemetry.addData("rightBackPower",rightBack.getPower());
+        telemetry.addData("rightFrontPower",rightFront.getPower());
         telemetry.addLine();
-        telemetry.addData("Sorter servo Pos: ", sorterServo.getPosition());
-        telemetry.addData("Sorter servo varPos: ", sorterServoPos);
+        telemetry.addData("Sorter servo Pos:",sorterServo.getPosition());
+        telemetry.addData("Sorter servo varPos:",sorterServoPos);
         telemetry.addLine();
-        // telemetry.addData("color sensor gain: ", colorSensor.getGain());
-        // telemetry.addData("color sensor alpha: ", colors.alpha);
-        // telemetry.addData("color sensor blue: ", colors.blue);
-        // telemetry.addData("color sensor red: ", colors.red);
-        // telemetry.addData("color sensor green: ", colors.green);
-        // telemetry.addLine();
-        telemetry.addData("intake activated: ", intakeActivated);
+        telemetry.addData("color sensor gain:", colorSensor.getGain());
+        telemetry.addData("color sensor alpha:", colors.alpha);
+        telemetry.addData("color sensor blue:", colors.blue);
+        telemetry.addData("color sensor red:", colors.red);
+        telemetry.addData("color sensor green:", colors.green);
 
         telemetry.update();
-
     }
-
 }
